a7da64a0edb74f8839e5df324a675089f1ac324c
wetsik <122281241+wetsik@users.noreply.github.com>
2025-06-01 18:10:50 +0500

    р

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = "http://localhost:8000/api";
+const API_BASE_URL = "https://wedding-venues.onrender.com/api";
 
 export const api = axios.create({
   baseURL: API_BASE_URL,
